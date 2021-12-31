1cc15d796b62db9545da53f84b967a723b1db99d
jonas sandström <jonas@gooddigitalmorning.com>
2021-12-31 11:06:44 +0100

    remove old folders

--- day1/b.py
+++
@@ -1,8 +0,0 @@
-i, counter, a = (0, 0, [int(i) for i in open('day.txt')])
-while True:
-    try:
-        slide, i = sum((a[i], a[i+1], a[i+2])), i+1
-        counter += int(slide < sum((a[i], a[i+1], a[i+2])))
-    except:
-        print(counter)
-        break
--- day7/b.py
+++
@@ -1,8 +0,0 @@
-with open("day.txt") as f:
-    crabs = [int(x) for x in f.readlines()[0].split(',')]
-    levels = {}
-    for level in range(min(crabs), max(crabs)+1):
-        levels[level] = sum([sum([x for x in range(0, abs(x - level)+1)]) for x in crabs])
-
-    best = min(levels, key=levels.get)
-    print(levels[best])
